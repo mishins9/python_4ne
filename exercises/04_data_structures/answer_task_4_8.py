9ffb035fdeb20e8678a47f26328031ca92522c72
mishins9 <donnerjack9@gmail.com>
2021-09-21 16:02:12 -0500

    All Task 4 ready

--- exercises/04_data_structures/answer_task_4_8.py
+++ exercises/04_data_structures/answer_task_4_8.py
@@ -24,11 +24,12 @@
 Это не значит, что задание сделано правильно, просто на данном этапе сложно иначе
 проверять результат.
 """
-
 ip = "192.168.3.1"
-oct1, oct2, oct3, oct4 = ip.split('.')
-template = '''
+
+octets = ip.split(".")
+
+output = """
 {0:<10}{1:<10}{2:<10}{3:<10}
-{0:08b}  {1:08b}  {2:08b}  {3:08b}
-    '''
-print(template.format(int(oct1), int(oct2), int(oct3), int(oct4)))
+{0:08b}  {1:08b}  {2:08b}  {3:08b}"""
+
+print(output.format(int(octets[0]), int(octets[1]), int(octets[2]), int(octets[3])))
