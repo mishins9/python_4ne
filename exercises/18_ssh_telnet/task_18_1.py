0f38f16916f42b12a9c924b6698bd79339f83fae
mishins9 <donnerjack9@gmail.com>
2021-11-14 15:28:05 -0600

    Check task 1 1a - run gns3

--- exercises/18_ssh_telnet/task_18_1.py
+++ exercises/18_ssh_telnet/task_18_1.py
@@ -18,7 +18,20 @@
 
 """
 import yaml
+from netmiko import (
+    ConnectHandler,
+    NetmikoTimeoutException,
+    NetmikoAuthenticationException,
+)
 
+def send_show_command(device, command):
+    try:
+        with ConnectHandler(**device) as ssh:
+            ssh.enable()
+            output = ssh.send_command(command)
+        return 'строку (output)с выводом команды'
+    except (NetmikoTimeoutException, NetmikoAuthenticationException) as error:
+        print(error)
 
 if __name__ == "__main__":
     command = "sh ip int br"
