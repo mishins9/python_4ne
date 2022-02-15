6fd7ec257fb415feef98b35afa8d4c34db77ba82
mishins9 <donnerjack9@gmail.com>
2022-02-15 05:55:08 -0600

    All Ready Task 21

--- exercises/21_textfsm/answer_task_21_5.py
+++ exercises/21_textfsm/answer_task_21_5.py
@@ -38,24 +38,27 @@
 Проверить работу функции на примере вывода команды sh ip int br
 и устройствах из devices.yaml.
 """
-
-import os
-from concurrent.futures import ThreadPoolExecutor
-import yaml
-import subprocess
-from itertools import repeat
 from task_21_4 import send_and_parse_show_command
+from concurrent.futures import ThreadPoolExecutor, as_completed
+from pprint import pprint
+import os
+import yaml
 
-def send_and_parse_command_parallel(devices, command, templates_path, limit = 3):
+
+def send_and_parse_command_parallel(devices, command, templates_path, limit=3):
     with ThreadPoolExecutor(max_workers=limit) as executor:
-        result = executor.map(send_and_parse_show_command, devices,
-                              repeat(command), repeat(templates_path))
-        l_out = {device['host']: output for device, output in zip(devices, result)}
-    return l_out
+        result_all = [
+            executor.submit(send_and_parse_show_command, device, command, templates_path)
+            for device in devices
+        ]
+        output = {device["host"]: f.result() for device, f in zip(devices, result_all)}
+    return output
 
-if __name__ == '__main__':
-    full_pth = os.path.join(os.getcwd(), "templates")
-    command = 'sh ip int br'
+
+if __name__ == "__main__":
     with open("devices.yaml") as f:
-        list_hosts = yaml.safe_load(f)
-    print(send_and_parse_command_parallel(list_hosts, command, full_pth))
+        devices = yaml.safe_load(f)
+    command = "sh ip int br"
+    path_dir = f"{os.getcwd()}/templates"
+    pprint(send_and_parse_command_parallel(devices, command, path_dir))
+
