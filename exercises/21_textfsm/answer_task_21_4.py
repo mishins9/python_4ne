57f096d594ba9d16bf02319395ab4c0cebf12f72
mishins9 <donnerjack9@gmail.com>
2022-02-15 01:18:06 -0600

    Task 21(4) add path with os

--- exercises/21_textfsm/answer_task_21_4.py
+++ exercises/21_textfsm/answer_task_21_4.py
@@ -22,30 +22,51 @@
 и устройствах из devices.yaml.
 """
 import os
-import yaml
+from pprint import pprint
 from netmiko import ConnectHandler
-from task_21_3 import parse_command_dynamic
+import yaml
 
 
-def send_and_parse_show_command(device_dict, command,
-                                templates_path, index='index'):
-    attr_dict = {'Command': command, 'Vendor': device_dict["device_type"]}
-    with ConnectHandler(**device_dict) as r1:
-        r1.enable()
-        output = r1.send_command(command)
-        result = parse_command_dynamic(output, attr_dict,
-                                       templ_path=templates_path)
-    return result
+def send_and_parse_show_command(device_dict, command, templates_path):
+    if "NET_TEXTFSM" not in os.environ:
+        os.environ["NET_TEXTFSM"] = templates_path
+    with ConnectHandler(**device_dict) as ssh:
+        ssh.enable()
+        output = ssh.send_command(command, use_textfsm=True)
+    return output
 
 
 if __name__ == "__main__":
     full_pth = os.path.join(os.getcwd(), "templates")
-    command = "sh ip int br"
     with open("devices.yaml") as f:
         devices = yaml.safe_load(f)
-
     for dev in devices:
-        res_dev = send_and_parse_show_command(dev, command,
-                                              templates_path=full_pth)
-        print(res_dev)
+        result = send_and_parse_show_command(
+            dev, "sh ip int br", templates_path=full_pth
+        )
+        pprint(result, width=120)
 
+# Второй вариант без использования use_textfsm в netmiko
+from task_21_3 import parse_command_dynamic
+
+
+def send_and_parse_show_command(device_dict, command, templates_path, index="index"):
+    attributes = {"Command": command, "Vendor": device_dict["device_type"]}
+    with ConnectHandler(**device_dict) as ssh:
+        ssh.enable()
+        output = ssh.send_command(command)
+        parsed_data = parse_command_dynamic(
+            output, attributes, templ_path=templates_path, index_file=index
+        )
+    return parsed_data
+
+
+if __name__ == "__main__":
+    full_pth = os.path.join(os.getcwd(), "templates")
+    with open("devices.yaml") as f:
+        devices = yaml.safe_load(f)
+    for dev in devices:
+        result = send_and_parse_show_command(
+            dev, "sh ip int br", templates_path=full_pth
+        )
+        pprint(result, width=120)
