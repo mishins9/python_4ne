ff16a9014a3ba71f5a9025747d8aab6ca541222a
python <python@debian>
2022-04-09 07:54:12 -0500

    Ready All Task 24

--- exercises/24_oop_inheritance/task_24_2d.py
+++ exercises/24_oop_inheritance/task_24_2d.py
@@ -32,3 +32,70 @@ ErrorInCommand                            Traceback (most recent call last)
 ...
 ErrorInCommand: При выполнении команды "lo" на устройстве 192.168.100.1 возникла ошибка "Incomplete command."
 """
+
+from netmiko.cisco.cisco_ios import CiscoIosSSH
+import re
+
+class ErrorInCommand(Exception):
+    """
+    Исключение генерируется, если при выполнении команды на оборудовании,
+    возникла ошибка.
+    """
+
+
+class MyNetmiko(CiscoIosSSH):
+    def __init__(self, **device_params):
+        super().__init__(**device_params)
+        self.enable()
+
+    def _check_error_in_command(self, command, result):
+        regex = "% (?P<err>.+)"
+        message = (
+                'При выполнении команды "{cmd}" на устройстве {device} '
+                'возникла ошибка "{error}"'
+        )
+        error_in_cmd = re.search(regex, result)
+        if error_in_cmd:
+            raise ErrorInCommand(
+                message.format(
+                cmd=command, device=self.host, error=error_in_cmd.group("err")
+                )
+            )
+
+    def send_command(self, command, *args, **kwargs):
+        command_output = super().send_command(command, *args, **kwargs)
+        self._check_error_in_command(command, command_output)
+        return command_output
+
+    def send_config_set(self, commands, ignore_errors=True, *args, **kwargs):
+
+        if ignore_errors:
+            output = super().send_config_set(commands, *args, **kwargs)
+            return output
+
+        if isinstance(commands, str):
+            commands = [commands]
+        commands_output = ""
+        self.config_mode()
+        for command in commands:
+            result = super().send_config_set(command, exit_config_mode=False)
+            commands_output += result
+            self._check_error_in_command(command, result)
+        self.exit_config_mode()
+        return commands_output
+
+        
+if __name__ == "__main__":
+
+    device_params = {
+        "device_type": "cisco_ios",
+        "ip": "192.168.100.1",
+        "username": "cisco",
+        "password": "cisco",
+        "secret": "cisco",
+    }
+
+    r1 = MyNetmiko(**device_params)
+    print(r1.send_config_set('lo', ignore_errors=False))
+
+
